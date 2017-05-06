3600319cf935e521e3d7773e3c9ef41a75801c62
F541U <F541U@DESKTOP-27T6CPM>
2017-05-06 09:26:49 +0100

    Criação das models

---
+++ 16804-Trabalho-Final-David-Violante/Models/Fornecedor.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _16804_Trabalho_Final_David_Violante.Models
+{
+    public class Fornecedor
+    {
+    }
+}
